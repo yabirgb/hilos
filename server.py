bc84f320ff1306f5fa0ed13dfb6999abad8c7d2c
yabir <yabirgb@gmail.com>
2017-08-29 18:27:24 +0200

    Render query

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 from flask import (Flask,render_template)
 
 from db.models_data import Tweet, Branch, calldb
@@ -6,14 +8,19 @@ app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 db = calldb()
 
-@app.route("/")
+@app.route("/about")
 def hello():
-    return render_template('home.html')
+    return render_template('about.html')
 
 @app.route("/manuel")
-def manuel():
-    q = Tweet.select().where(Tweet.branch==3).order_by(Tweet.tweet_id)[:100]
-    return render_template('lista.html', tweets=q)
+@app.route("/manuel/<int:page>")
+def manuel(page=1):
+    per_page = 40
+    query = Tweet.select().where(Tweet.branch==4).order_by(Tweet.tweet_id)
+    pages = int(ceil(float(query.count()) / per_page))
+
+    result = query.paginate(page, per_page)
+    return render_template('lista.html', tweets=result, page=page, pages=pages)
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
